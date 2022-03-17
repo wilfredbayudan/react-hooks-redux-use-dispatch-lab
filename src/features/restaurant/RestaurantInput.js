6c4f5a4e8b6c8720be8b32b1f8387ad008a8382c
Wilfred <wilfredbayudan@gmail.com>
2022-03-16 15:33:43 -1000

    Completed assignment

--- src/features/restaurant/RestaurantInput.js
+++ src/features/restaurant/RestaurantInput.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import { addRestaurant } from "./restaurantsSlice";
+import { useDispatch } from "react-redux";
 
 function RestaurantInput() {
   const [name, setName] = useState("");
   const [location, setLocation] = useState("");
+
+  const dispatch = useDispatch();
 
   function handleNameChange(event) {
     setName(event.target.value);
@@ -16,6 +19,8 @@ function RestaurantInput() {
   function handleSubmit(event) {
     event.preventDefault();
     // add missing code
+    const restaurant = { name, location };
+    dispatch(addRestaurant(restaurant));
   }
 
   return (
